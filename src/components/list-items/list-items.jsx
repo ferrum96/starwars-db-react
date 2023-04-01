6862f145b22cf82952885229287a0b6d6318eced
Константин <51968997+ferrum96@users.noreply.github.com>
2023-04-01 21:28:31 +0500

    addded logic for get data api

--- src/components/list-items/list-items.jsx
+++ src/components/list-items/list-items.jsx
@@ -1,20 +1,56 @@
 import { Component } from "react";
 
 import "./list-items.css"
+import Loader from "../loader/loader";
+import SwapiService from "../../services/swapi-service";
 
 export default class ListItems extends Component {
+
+	swapiService = new SwapiService();
+
+	state = {
+		listItems: [],
+		loading: true
+	}
+
+	componentDidMount() {
+		this.swapiService
+			.getAllPeople()
+			.then(this.onLoadedPerson)
+			.catch(this.onError)
+	}
+
+	onLoadedPerson = (listItems) => {
+		this.setState({
+			listItems,
+			loading: false
+		})
+	}
+
+	onError = () => {
+		this.setState({
+			error: true,
+			loading: false
+		})
+	}
+
 	render() {
+		const { listItems, loading, error } = this.state;
+
+		const items = listItems.map(({id, name}) => {
+			return (
+				<li 
+				key={id} 
+				className="list-group-item"
+				onClick={() => this.props.onSelectedItem(id)}>
+					{name}
+				</li>
+			)
+		});
+
 		return (
 			<ul className="item-list list-group">
-				<li className="list-group-item">
-					Luke Skywalker
-				</li>
-				<li className="list-group-item">
-					Darth Vader
-				</li>
-				<li className="list-group-item">
-					R2-D2
-				</li>
+				{error ? null : loading ? <Loader /> : items}
 			</ul>
 		)
 	}
