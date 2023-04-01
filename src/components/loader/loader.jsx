3bc65f0e98108decfb1412de1bfd65005be755ca
Константин <51968997+ferrum96@users.noreply.github.com>
2023-04-01 14:04:35 +0500

    added loader component

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+import "./loader.css"
+
+const Loader = () => {
+	return (
+		<div className="loadingio-spinner-double-ring-3lynfclh1k5">
+			<div className="ldio-dne7hzce75">
+				<div></div>
+				<div></div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Loader;
+
