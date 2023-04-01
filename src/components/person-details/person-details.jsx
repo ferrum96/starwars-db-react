2ed64151a07bb5391c09547670b184b695ebc676
Константин <51968997+ferrum96@users.noreply.github.com>
2023-04-01 22:26:57 +0500

    added prop

--- src/components/person-details/person-details.jsx
+++ src/components/person-details/person-details.jsx
@@ -3,25 +3,29 @@ import { Component } from "react";
 import "./person-details.css"
 
 export default class PersonDetails extends Component {
+
 	render() {
+
+		const { person } = this.props;
+
 		return (
 			<div className="person-details card">
 				<img className="person-image"
-					src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+					src={`https://starwars-visualguide.com/assets/img/characters/${person.id}.jpg`} alt={`character ${person.id}`} />
 				<div className="card-body">
-					<h4>R2-D2</h4>
+					<h4>{person.name}</h4>
 					<ul className="list-group list-group-flush">
 						<li className="list-group-item">
 							<span className="term">Gender</span>
-							<span>male</span>
+							<span>{person.gender}</span>
 						</li>
 						<li className="list-group-item">
 							<span className="term">Birth Year</span>
-							<span>43</span>
+							<span>{person.birthYear}</span>
 						</li>
 						<li className="list-group-item">
 							<span className="term">Eye Color</span>
-							<span>red</span>
+							<span>{person.eyeColor}</span>
 						</li>
 					</ul>
 				</div>
