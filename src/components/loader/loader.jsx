4414de5acf9c9a5de1f07193e59bb4f40b6b96bc
Константин <51968997+ferrum96@users.noreply.github.com>
2023-04-01 17:17:35 +0500

    refacrored loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -4,19 +4,27 @@ import "./loader.css"
 
 const Loader = () => {
 	return (
-		<div className="loadingio-spinner-double-ring-3lynfclh1k5">
-			<div className="ldio-dne7hzce75">
-				<div></div>
-				<div></div>
-				<div>
-					<div></div>
-				</div>
-				<div>
-					<div></div>
-				</div>
-			</div>
-		</div>
-	);
+
+		<div className="lds-css">
+      <div className="lds-double-ring">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+
+		// <div className="loadingio-spinner-double-ring-3lynfclh1k5">
+		// 	<div className="ldio-dne7hzce75">
+		// 		<div></div>
+		// 		<div></div>
+		// 		<div>
+		// 			<div></div>
+		// 		</div>
+		// 		<div>
+		// 			<div></div>
+		// 		</div>
+		// 	</div>
+		// </div>
 }
 
 export default Loader;
