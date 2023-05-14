274009951dc96ec8d692d7847fd28fa06c710086
Константин <51968997+ferrum96@users.noreply.github.com>
2023-05-14 19:12:53 +0300

    refactored links

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,19 +8,19 @@ export default class Header extends Component {
 		return (
 			<div className="header d-flex">
 				<h3>
-					<a href="#">
+					<a href="/">
 						Star DB
 					</a>
 				</h3>
 				<ul className="d-flex">
 					<li>
-						<a href="#">People</a>
+						<a href="people">People</a>
 					</li>
 					<li>
-						<a href="#">Planets</a>
+						<a href="planets">Planets</a>
 					</li>
 					<li>
-						<a href="#">Starships</a>
+						<a href="starships">Starships</a>
 					</li>
 				</ul>
 			</div>
